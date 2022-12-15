9807e64333e46bce2675bfc3ce82c9fe10cbac99
E Mc <36801051+13EdMc07@users.noreply.github.com>
2022-12-15 13:13:58 -0600

    Add files via upload

---
+++ Day-4-Drawing-Within-the-Lines-using-Conditionals/src/sketchpad/SketchPad.java
@@ -0,0 +1,56 @@
+package sketchpad;
+
+import buttons.*;
+import tools.*;
+
+public class SketchPad {
+    private final Screen screen = new Screen();
+    private final MouseInfo mouseInfo = new MouseInfo();
+    private final PaintingTool paint = new PaintingTool();
+
+    // Declaring variables to represent the last location of the mouse
+    private int lastMouseX;
+    private int lastMouseY;
+
+    public void initialLaunch() {
+        // The background color
+        screen.setBackgroundColor(Color.CYAN);
+
+        // The interior sketch surface
+        paint.setFillColor(Color.WHITE);
+        paint.drawRect(30, 30, 540, 340);
+    }
+
+    public void update() {
+        // Setting the pen color to a random number
+        paint.setLineColor(
+               Generator.randomInt(256),
+               Generator.randomInt(256),
+               Generator.randomInt(256)
+        );
+
+        if (mouseInfo.isMousePressed()) {
+            lastMouseX = mouseInfo.getX();
+            lastMouseY = mouseInfo.getY();
+        }
+        
+        // Drawing a line from the current mouse location to the last mouse location
+      if (mouseInfo.isMouseDragged() &&
+          mouseInfo.getX() > 30 &&
+          mouseInfo.getY() > 30 &&
+          mouseInfo.getX() < 570 &&
+          mouseInfo.getY() < 370) { 
+          
+        
+        
+        paint.drawLine(mouseInfo.getX(),
+                         mouseInfo.getY(),
+                         lastMouseX,
+                         lastMouseY);
+      }
+        // Updating the variables of the last mouse location
+        lastMouseX = mouseInfo.getX();
+        lastMouseY = mouseInfo.getY();
+
+    }
+}

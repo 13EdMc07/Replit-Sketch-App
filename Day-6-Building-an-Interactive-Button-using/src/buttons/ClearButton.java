f6cd305d52407c844808eb81110e9ecc56bdd968
E Mc <36801051+13EdMc07@users.noreply.github.com>
2022-12-19 15:58:10 -0600

    Add files via upload

---
+++ Day-6-Building-an-Interactive-Button-using/src/buttons/ClearButton.java
@@ -0,0 +1,85 @@
+ package buttons;
+
+import tools.Color;
+import tools.MouseInfo;
+import tools.PaintingTool;
+
+public class ClearButton {
+    private MouseInfo mouseInfo = new MouseInfo();
+    private PaintingTool paint = new PaintingTool();
+
+    private int leftX;
+    private int topY;
+    private int size;
+    private Color color;
+
+    public ClearButton(int incomingX,
+                       int incomingY,
+                       int incomingSize,
+                       Color incomingColor) {
+      
+      leftX = incomingX;
+      topY = incomingY;
+      size = incomingSize;
+      color = incomingColor;
+      
+    }
+
+    public void drawSelf() {
+      // Draw a Rectangle 
+
+      //Black border
+      //Uee the color value to shade the interior of the rectangle
+     
+        
+
+      //Draw the rectangle based on thr properties of this clear button
+      
+      paint.setLineColor(Color.BLACK);
+      paint.setFillColor(color);
+      paint.drawRect(leftX, topY, size, size);
+
+      paint.setLineColor(Color.RED);
+      paint.drawLine(leftX, topY, leftX + size, topY + size);
+
+        
+    }
+
+    public boolean isPressed() {
+        return mouseInfo.isMousePressed() &&
+                mouseInfo.getX() >= leftX &&
+                mouseInfo.getX() <= leftX + size &&
+                mouseInfo.getY() >= topY &&
+                mouseInfo.getY() <= topY + size;
+    }
+  
+    public void update() {
+        if (isPressed()) {
+            performAction();
+        }
+    }
+
+
+    public void performAction() {
+        paint.setFillColor(Color.WHITE);
+        paint.drawRect(30, 30, 540, 340);
+    }
+
+  
+    //Getters
+    public Color getColor() {
+        return color;
+    }
+
+    public int getLeftX() {
+        return leftX;
+    }
+
+    public int getTopY() {
+        return topY;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
---
+++ Day-6-Building-an-Interactive-Button-using/src/sketchpad/SketchPad.java
@@ -0,0 +1,86 @@
+package sketchpad;
+
+import buttons.*;
+import tools.*;
+
+public class SketchPad {
+    private final Screen screen = new Screen();
+    private final MouseInfo mouseInfo = new MouseInfo();
+    private final PaintingTool paint = new PaintingTool();
+
+    // Declaring variables to represent the last location of the mouse
+    private int lastMouseX;
+    private int lastMouseY;
+
+    private ClearButton clearButton = new ClearButton(
+      5,
+      5,
+      20,
+      Color.WHITE);
+    //unneeded button
+    /*private ClearButton clearButton2 = new ClearButton(
+      50,
+      100,
+      50,
+      Color.LIME);*/
+
+    public void initialLaunch() {
+        // The background color
+        screen.setBackgroundColor(Color.CYAN);
+
+        // Generate many random circles for a colorful backdground
+        for (int circleNumber = 0; circleNumber < 500; circleNumber++) {
+            drawRandomCircle();
+        }
+
+        // The interior sketch surface
+        paint.setFillColor(Color.WHITE);
+        paint.drawRect(30, 30, 540, 340);
+
+        clearButton.drawSelf();
+        
+        //unneeded button
+        //clearButton2.drawSelf();
+
+    }
+
+    public void update() {
+        // Setting the pen color to a random number
+        paint.setLineColor(
+               Generator.randomInt(256),
+               Generator.randomInt(256),
+               Generator.randomInt(256)
+        );
+        
+        // If the mouse was just clicked, update the last mouse location data
+        if (mouseInfo.isMousePressed()) {
+            lastMouseX = mouseInfo.getX();
+            lastMouseY = mouseInfo.getY();
+        }
+
+        // Only draw lines if the mouse was dragged and inside the interior sketch surface
+        if (mouseInfo.isMouseDragged() &&
+            mouseInfo.getX() >= 30 &&
+            mouseInfo.getX() <= 570 &&
+            mouseInfo.getY() >= 30 &&
+            mouseInfo.getY() <= 370) {
+
+            paint.drawLine(mouseInfo.getX(), mouseInfo.getY(), lastMouseX, lastMouseY);
+            lastMouseX = mouseInfo.getX();
+            lastMouseY = mouseInfo.getY();
+        }
+        clearButton.update();
+
+    }
+
+    public void drawRandomCircle() {
+        int x = Generator.randomInt(600);
+        int y = Generator.randomInt(400);
+        int radius = Generator.randomInt(10, 30);
+        int r = Generator.randomInt(255);
+        int g = Generator.randomInt(255);
+        int b = Generator.randomInt(255);
+        paint.setFillColor(r, g, b);
+        paint.drawCircle(x, y, radius);
+    }
+}
